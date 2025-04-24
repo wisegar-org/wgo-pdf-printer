cdd530c9a222d667e96c8537b1cf393700a9bb16
Yariel Rodriguez <yariel.re@gmail.com>
2025-04-24 16:12:44 +0200

    Update package files and index exports

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,16 @@ export * from './services/ExportPdfService';
 export * from './services/file.service';
 export * from './services/pdf.service';
 
-async function runDemo() {
-  const sourceDocuments = ['1.pdf', '2.pdf', '3.pdf', '4.pdf'];
-  for (let index = 0; index < sourceDocuments.length; index++) {
-    const file = sourceDocuments[index];
-    await createDocument(file);
-  }
-  const outputDocument = 'output.pdf';
-  await mergeDocuments(sourceDocuments, outputDocument);
+// async function runDemo() {
+//   const sourceDocuments = ['1.pdf', '2.pdf', '3.pdf', '4.pdf'];
+//   for (let index = 0; index < sourceDocuments.length; index++) {
+//     const file = sourceDocuments[index];
+//     await createDocument(file);
+//   }
+//   const outputDocument = 'output.pdf';
+//   await mergeDocuments(sourceDocuments, outputDocument);
 
-  const result = await zipFiles(sourceDocuments, 'output.zip');
-  console.log(`Zipped to: ${result}`);
-}
-runDemo();
+//   const result = await zipFiles(sourceDocuments, 'output.zip');
+//   console.log(`Zipped to: ${result}`);
+// }
+// runDemo();
